81d82fe77b2986b413f1da08f82524b3d76017bc
margarita <margarita.koorkina@mail.ru>
2016-06-02 13:53:56 +0300

    v.0.1

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,9 +3,9 @@ class LineItem < ActiveRecord::Base
   belongs_to :book
   belongs_to :cart
   
-  #validates :quantity, {
-    #presence: true,
-    #:numericality => { :greater_than_or_equal_to => 1, :message => "Check quantity of books that you want to add to your cart"}
-    #}
+  validates :quantity, {
+    presence: true,
+    :numericality => { :greater_than_or_equal_to => 1, :message => "Check quantity of books that you want to add to your cart"}
+  }
 
 end
