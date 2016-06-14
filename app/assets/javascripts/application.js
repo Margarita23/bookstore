9765f4d87ecc042c11a2460e1da62d543a68bcf3
margarita <margarita.koorkina@mail.ru>
2016-06-14 16:48:33 +0300

    v.0.2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -65,10 +65,7 @@ $(document).ready(function() {
   function statusChangeCallback(response) {
     console.log('statusChangeCallback');
     console.log(response);
-    // The response object is returned with a status field that lets the
-    // app know the current login status of the person.
-    // Full docs on the response object can be found in the documentation
-    // for FB.getLoginStatus().
+   
     if (response.status === 'connected') {
       // Logged into your app and Facebook.
       testAPI();
@@ -84,9 +81,6 @@ $(document).ready(function() {
     }
   }
 
-  // This function is called when someone finishes with the Login
-  // Button.  See the onlogin handler attached to it in the sample
-  // code below.
   function checkLoginState() {
     FB.getLoginStatus(function(response) {
       statusChangeCallback(response);
@@ -101,18 +95,6 @@ $(document).ready(function() {
     xfbml      : true,  // parse social plugins on this page
     version    : 'v2.2' // use version 2.2
   });
-
-  // Now that we've initialized the JavaScript SDK, we call 
-  // FB.getLoginStatus().  This function gets the state of the
-  // person visiting this page and can return one of three states to
-  // the callback you provide.  They can be:
-  //
-  // 1. Logged into your app ('connected')
-  // 2. Logged into Facebook, but not your app ('not_authorized')
-  // 3. Not logged into Facebook and can't tell if they are logged into
-  //    your app or not.
-  //
-  // These three cases are handled in the callback function.
 
   FB.getLoginStatus(function(response) {
     statusChangeCallback(response);
