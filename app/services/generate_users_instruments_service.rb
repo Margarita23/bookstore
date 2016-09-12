1cccccf90dd6a64f1ec928b2ca7641d3f83ff9f5
margarita <margarita.korkina@mail.ru>
2016-09-12 16:41:07 +0300

    ver

--- app/services/generate_users_instruments_service.rb
+++ app/services/generate_users_instruments_service.rb
@@ -5,13 +5,17 @@ class GenerateUsersInstrumentsService
 
   def call
     if !Address.exists?(user_billing_id: @user.id)
-      billing_address = Address.create(user_billing_id: @user.id)
+      billing_address = Address.create(user_billing_id: @user.id, first_name: @user.first_name, last_name: @user.last_name)
     end
     if !Address.exists?(user_shipping_id: @user.id)
-      shipping_address = Address.create(user_shipping_id: @user.id)
+      shipping_address = Address.create(user_shipping_id: @user.id, first_name: @user.first_name, last_name: @user.last_name)
     end
     if !Cart.exists?(user_id: @user.id) && !@user.admin
       cart = Cart.create(id: @user.id, user_id: @user.id)
     end
+  end
+  
+  def facebook_info
+    User.from_omniauth(request.env["omniauth.auth"])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160826202235) do
+ActiveRecord::Schema.define(version: 20160912133754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -273,6 +273,8 @@ ActiveRecord::Schema.define(version: 20160826202235) do
     t.string   "uid"
     t.boolean  "admin",                  default: false
     t.string   "locale"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
