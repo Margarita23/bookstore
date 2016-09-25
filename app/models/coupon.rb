cb6a5e60ac443d77df59a0a0f3733afb6e630799
Margarita <margarita.korkina@mail.ru>
2016-09-25 11:15:44 +0300

    after_checking

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -2,4 +2,7 @@ class Coupon < ActiveRecord::Base
   belongs_to :order
   belongs_to :user
   belongs_to :cart
+  def reset_coupon_params(order_id)
+    self.update(user_id: nil, cart_id: nil, order_id: order_id)
+  end
 end
