7e6076fc3661e298dbf6f09273298ef36a866ac6
Margarita <margarita.korkina@mail.ru>
2016-09-25 09:52:42 +0300

    after_checking

--- app/services/generate_users_instruments_service.rb
+++ app/services/generate_users_instruments_service.rb
@@ -4,13 +4,13 @@ class GenerateUsersInstrumentsService
   end
 
   def call
-    if !Address.exists?(user_billing_id: @user.id)
+    unless Address.exists?(user_billing_id: @user.id)
       billing_address = Address.create(user_billing_id: @user.id, first_name: @user.first_name, last_name: @user.last_name)
     end
-    if !Address.exists?(user_shipping_id: @user.id)
+    unless Address.exists?(user_shipping_id: @user.id)
       shipping_address = Address.create(user_shipping_id: @user.id, first_name: @user.first_name, last_name: @user.last_name)
     end
-    if !Cart.exists?(user_id: @user.id) && !@user.admin
+    unless Cart.exists?(user_id: @user.id) && !@user.admin
       cart = Cart.create(id: @user.id, user_id: @user.id)
     end
   end
