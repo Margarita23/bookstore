f0111736e0f76e8f8d62bc10b44aa770da449245
margarita <margarita.korkina@mail.ru>
2016-09-26 16:13:34 +0300

    checking

--- app/services/generate_users_instruments_service.rb
+++ app/services/generate_users_instruments_service.rb
@@ -11,7 +11,9 @@ class GenerateUsersInstrumentsService
     unless Address.exists?(user_shipping_id: @user.id)
       Address.create(user_shipping_id: @user.id, first_name: @user.first_name, last_name: @user.last_name)
     end
-    Cart.create(id: @user.id, user_id: @user.id) unless checking_cart
+    unless checking_cart
+      Cart.create(id: @user.id, user_id: @user.id)
+    end
   end
 
   def checking_cart
