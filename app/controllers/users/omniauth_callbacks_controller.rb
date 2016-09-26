ec08376ebbfdda09a4a86c91d3a42b7c31a76a3c
margarita <margarita.korkina@mail.ru>
2016-09-26 12:50:39 +0300

    checking

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,8 +6,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, event: :authentication
       GenerateUsersInstrumentsService.new(@user).call
       set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
-    elsif request.env["omniauth.auth"].info.email.blank?
-      redirect_to "/users/auth/facebook?auth_type=rerequest&scope=email"
     else
       session['devise.facebook_data'] = request.env['omniauth.auth']
       redirect_to new_user_registration_url
