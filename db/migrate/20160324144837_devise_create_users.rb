aa12b0a8ef289f24df238742df90a50f862207e2
margarita <margarita.korkina@mail.ru>
2016-09-26 04:43:51 +0300

    checking

--- db/migrate/20160324144837_devise_create_users.rb
+++ db/migrate/20160324144837_devise_create_users.rb
@@ -2,7 +2,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
 
-      t.string :email,              null: false, default: ""
+      t.string :email,              null: true, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       t.string   :reset_password_token
